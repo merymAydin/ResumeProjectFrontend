c66dc2cb2124b203670552169713c64ebf5de118
Meryem Aydın <aydnn.meryem@gmail.com>
2025-08-07 19:38:00 +0300

    Refactor index.html and scripts.js paths for asset loading; wrap App component in PersonalInfoContextProvider in main.jsx for improved state management.

--- ResumeProjectFrontend/src/main.jsx
+++ ResumeProjectFrontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PersonalInfoContextProvider } from './Contexts/PersonalInfoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PersonalInfoContextProvider>
+      <App />
+    </PersonalInfoContextProvider>
   </StrictMode>,
 )
