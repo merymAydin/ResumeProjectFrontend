07293ee9b10a662d2bc01b2fbc9eb51f3dd6dc0c
Meryem Aydın <aydnn.meryem@gmail.com>
2026-07-24 23:39:58 +0300

    Update ContactContext.jsx

--- ResumeProjectFrontend/src/Contexts/ContactContext.jsx
+++ ResumeProjectFrontend/src/Contexts/ContactContext.jsx
@@ -8,7 +8,7 @@ const ContactContextProvider = ({children}) => {
     useEffect(() => {
         const getContacts = async() => {
             try {
-                const response = await fetch("https://localhost:7156/api/Contacts");
+                const response = await fetch("https://resume-project-agsn.onrender.com");
                 const data = await response.json();
                 setContacts(data);
             } catch (error) {
