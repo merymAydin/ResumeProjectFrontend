7f2392d840e01586e33a1fdbf149e53038f511b5
Meryem Aydın <aydnn.meryem@gmail.com>
2026-07-24 23:39:35 +0300

    Update AboutContext.jsx

--- ResumeProjectFrontend/src/Contexts/AboutContext.jsx
+++ ResumeProjectFrontend/src/Contexts/AboutContext.jsx
@@ -8,7 +8,7 @@ const AboutContextProvider = ({children}) => {
     useEffect(()=> {
         const getAbouts = async() =>{
             try {
-                const response = await fetch("https://localhost:7156/api/Abouts");
+                const response = await fetch("https://resume-project-agsn.onrender.com");
                 const data = await response.json();
                 setAbouts(data);
             } catch (error) {
